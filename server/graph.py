a2b3b591ebda262733c8e67565e757810a4564d5
Артем Маршалкин <>
2018-02-07 21:33:01 +0300

    R&D graph

---
+++ server/graph.py
@@ -0,0 +1,10 @@
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import networkx as nx
+DG = nx.DiGraph()
+DG.add_weighted_edges_from([('amarshalkin', 'infom', 200), ('klimov', 'amarshalkin', 300), ('fedorov', 'amarshalkin', 150), ('fedorov', 'klimov', 50)])
+nx.draw(DG, with_labels=True, font_weight='bold')
+plt.savefig("graph.png")
+
+list(nx.simple_cycles(DG))
