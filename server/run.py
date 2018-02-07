289525ccd74a823c70a41f5c1f53b0ead1987328
marshalkin <artem.marshalkin@gmail.com>
2018-02-07 17:48:22 +0300

    other

--- server/run.py
+++ server/run.py
@@ -5,7 +5,7 @@ app = Eve(settings='settings.py')
 
 @app.route('/docs')
 def index():
-    rreturn app.send_static_file('api.html')
+    return app.send_static_file('api.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
