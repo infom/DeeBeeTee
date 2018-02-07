bcb23f3f433e7c4d3f4f7acd3ff4a77751c07a5a
Артем Маршалкин <>
2018-02-07 22:11:56 +0300

    other

--- server/graph.py
+++ server/graph.py
@@ -5,12 +5,14 @@ import networkx as nx
 
 DG = nx.DiGraph()
 DG.add_weighted_edges_from([('amarshalkin', 'infom', 200), ('klimov', 'amarshalkin', 300), ('amarshalkin', 'fedorov', 150), ('fedorov', 'klimov', 50)])
-plt.subplot(211)
+ax = plt.subplot(211)
+ax.set_title("Direct Graph")
 nx.draw_shell(DG, with_labels=True, font_weight='bold')
 
 G = nx.MultiDiGraph()
-G.add_weighted_edges_from([('amarshalkin', 'infom', 200), ('klimov', 'amarshalkin', 300), ('amarshalkin', 'fedorov', 150), ('fedorov', 'klimov', 50)])
-plt.subplot(212)
+G.add_weighted_edges_from([('amarshalkin', 'infom', 200),('amarshalkin', 'infom', 100), ('klimov', 'amarshalkin', 300), ('amarshalkin', 'fedorov', 150), ('fedorov', 'klimov', 50)])
+ax1 = plt.subplot(212)
+ax1.set_title("Multi Direct Graph")
 nx.draw_shell(G, with_labels=True, font_weight='bold')
 
 plt.savefig("graphs.png")
