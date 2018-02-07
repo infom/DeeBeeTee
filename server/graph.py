5b00b263ce7152bb88fc848ee8234789ed4e3323
Артем Маршалкин <>
2018-02-08 00:10:27 +0300

    other

--- server/graph.py
+++ server/graph.py
@@ -13,6 +13,7 @@ sc = list(nx.simple_cycles(DG))
 
 nx.draw_shell(DG, with_labels=True, font_weight='bold')
 for cycle in sc:
+    print('find simple cycle from direct graph: ' + cycle)
     nx.draw_networkx_nodes(DG,pos,
                        nodelist=cycle,
                        node_color='b',
@@ -30,6 +31,7 @@ sc = list(nx.simple_cycles(MDG))
 
 nx.draw_shell(MDG, with_labels=True, font_weight='bold')
 for cycle in sc:
+    print('find simple cycle from multi direct graph: ' + cycle)
     nx.draw_networkx_nodes(MDG,pos,
                        nodelist=cycle,
                        node_color='b',
