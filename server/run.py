93c1f7893ef44aa957da6b583598409e8f08f693
marshalkin <artem.marshalkin@gmail.com>
2018-02-07 18:25:36 +0300

    other

--- server/run.py
+++ server/run.py
@@ -1,13 +1,19 @@
 import os
 from eve import Eve
 from flask import render_template
+import jinja2
+
+my_loader = jinja2.ChoiceLoader([
+    app.jinja_loader,
+    jinja2.FileSystemLoader(os.getcwd()),
+])
 
 app = Eve(settings='settings.py')
+app.jinja_loader = my_loader
 
 @app.route('/')
 def index():
-    path = os.path.abspath('api.html')
-    return render_template(path)
+    return render_template('api.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
