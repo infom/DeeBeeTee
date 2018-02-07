7e398f23168b36b22d8ccb1e908663d841ff30e1
marshalkin <artem.marshalkin@gmail.com>
2018-02-07 18:00:28 +0300

    other

--- server/run.py
+++ server/run.py
@@ -1,11 +1,10 @@
 from eve import Eve
-from flask import render_template
 
 app = Eve(settings='settings.py')
 
 @app.route('/')
 def index():
-    return app.send_static_file('api.html')
+    return 'Hello World!'
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
