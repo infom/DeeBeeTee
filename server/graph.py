c74179fd181280e1cb64eefc086a9d628e9421c1
Артем Маршалкин <>
2018-02-07 22:26:56 +0300

    other

--- server/graph.py
+++ server/graph.py
@@ -10,7 +10,7 @@ ax.set_title("Direct Graph")
 nx.draw_shell(DG, with_labels=True, font_weight='bold')
 
 G = nx.MultiDiGraph()
-G.add_weighted_edges_from([('amarshalkin', 'infom', 200, {'color':'blue'}),('amarshalkin', 'infom', 100, {'color':'red'}), ('klimov', 'amarshalkin', 300), ('amarshalkin', 'fedorov', 150), ('fedorov', 'klimov', 50)])
+G.add_weighted_edges_from([('amarshalkin', 'infom', 200),('amarshalkin', 'infom', 100), ('klimov', 'amarshalkin', 300), ('amarshalkin', 'fedorov', 150), ('fedorov', 'klimov', 50)])
 ax1 = plt.subplot(212)
 ax1.set_title("Multi Direct Graph")
 nx.draw_shell(G, with_labels=True, font_weight='bold')
