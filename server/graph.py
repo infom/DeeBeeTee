3b2a49846d8355329d204899313638875dfe3b0d
Артем Маршалкин <>
2018-02-07 23:53:43 +0300

    other

--- server/graph.py
+++ server/graph.py
@@ -5,20 +5,22 @@ import networkx as nx
 
 DG = nx.DiGraph()
 DG.add_weighted_edges_from([('amarshalkin', 'infom', 200), ('klimov', 'amarshalkin', 300), ('amarshalkin', 'fedorov', 150), ('fedorov', 'klimov', 50)])
-
 pos=nx.spring_layout(DG) # positions for all nodes
 
 ax = plt.subplot(211)
 ax.set_title("Direct Graph")
 sc = list(nx.simple_cycles(DG))
-nx.draw_shell(DG, with_labels=True, font_weight='bold')
 
 for cycle in sc:
     nx.draw_networkx_nodes(DG,pos,
                        nodelist=cycle,
-                       node_color='r',
+                       node_color='b',
                        node_size=500,
                        alpha=0.8)
+
+nx.draw_shell(DG, pos, with_labels=True, font_weight='bold')
+
+
 
 G = nx.MultiDiGraph()
 G.add_weighted_edges_from([('amarshalkin', 'infom', 200),('amarshalkin', 'infom', 100), ('klimov', 'amarshalkin', 300), ('amarshalkin', 'fedorov', 150), ('fedorov', 'klimov', 50)])
