f8716a4cd7ed3ad92e991c130ffd95f9e1f55300
marshalkin <artem.marshalkin@gmail.com>
2018-02-07 18:15:26 +0300

    other

--- server/run.py
+++ server/run.py
@@ -1,11 +1,13 @@
+import os
 from eve import Eve
 from flask import render_template
 
 app = Eve(settings='settings.py')
 
-@app.route('/')
+@app.route('/' methods=['GET'])
 def index():
-    return render_template("api.html")
+    path = os.path.abspath('api.html')
+    return render_template(path)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
