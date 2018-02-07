86d0c0d3381466d11d445fa58004b223819c3838
marshalkin <artem.marshalkin@gmail.com>
2018-02-07 16:58:49 +0300

    api docs router

--- server/run.py
+++ server/run.py
@@ -1,5 +1,9 @@
 from eve import Eve
 app = Eve(settings='settings.py')
 
+@app.route('/docs')
+def root():
+    return app.send_static_file('api.html')
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
