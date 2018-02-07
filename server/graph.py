2770883ce0ebfd4a78c824a50acda52219b63513
Артем Маршалкин <>
2018-02-07 22:08:45 +0300

    other

--- server/graph.py
+++ server/graph.py
@@ -6,11 +6,11 @@ import networkx as nx
 DG = nx.DiGraph()
 DG.add_weighted_edges_from([('amarshalkin', 'infom', 200), ('klimov', 'amarshalkin', 300), ('amarshalkin', 'fedorov', 150), ('fedorov', 'klimov', 50)])
 plt.subplot(211)
-nx.draw(DG, with_labels=True, font_weight='bold')
+nx.draw_shell(DG, with_labels=True, font_weight='bold')
 
 G = nx.MultiDiGraph()
 G.add_weighted_edges_from([('amarshalkin', 'infom', 200), ('klimov', 'amarshalkin', 300), ('amarshalkin', 'fedorov', 150), ('fedorov', 'klimov', 50)])
 plt.subplot(212)
-nx.draw(G, with_labels=True, font_weight='bold')
+nx.draw_shell(G, with_labels=True, font_weight='bold')
 
 plt.savefig("graphs.png")
