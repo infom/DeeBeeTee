66b1f4e39fcfa543737c439e6dcf2c84e3f66b33
Артем Маршалкин <>
2018-02-07 23:44:18 +0300

    other

--- server/graph.py
+++ server/graph.py
@@ -5,20 +5,27 @@ import networkx as nx
 
 DG = nx.DiGraph()
 DG.add_weighted_edges_from([('amarshalkin', 'infom', 200), ('klimov', 'amarshalkin', 300), ('amarshalkin', 'fedorov', 150), ('fedorov', 'klimov', 50)])
+
+pos=nx.spring_layout(DG) # positions for all nodes
+
 ax = plt.subplot(211)
 ax.set_title("Direct Graph")
 sc = list(nx.simple_cycles(DG))
-ax.text(0.95, 0.01, sc, verticalalignment='bottom', horizontalalignment='center',)
 nx.draw_shell(DG, with_labels=True, font_weight='bold')
-
-print(sc)
+nx.draw_networkx_nodes(DG,pos,
+                       nodelist=sc,
+                       node_color='r',
+                       node_size=500,
+                       alpha=0.8)
 
 G = nx.MultiDiGraph()
 G.add_weighted_edges_from([('amarshalkin', 'infom', 200),('amarshalkin', 'infom', 100), ('klimov', 'amarshalkin', 300), ('amarshalkin', 'fedorov', 150), ('fedorov', 'klimov', 50)])
 ax1 = plt.subplot(212)
 ax1.set_title("Multi Direct Graph")
 nx.draw_shell(G, with_labels=True, font_weight='bold')
-sc = list(nx.simple_cycles(DG))
-print(sc)
 
 plt.savefig("graphs.png")
+
+
+
+# nodes
