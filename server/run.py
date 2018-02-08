d867ed499400bc49e1ac55880b470b22d62fa388
marshalkin <artem.marshalkin@gmail.com>
2018-02-08 09:11:03 +0300

    add graph page

--- server/run.py
+++ server/run.py
@@ -3,20 +3,22 @@ from eve import Eve
 from flask import render_template
 import jinja2
 
-
-
 app = Eve(settings='settings.py')
 
 loader = jinja2.ChoiceLoader([
     app.jinja_loader,
-    jinja2.FileSystemLoader(os.getcwd()+'/docs/api'),
+    jinja2.FileSystemLoader(os.getcwd()),
 ])
 
 app.jinja_loader = loader
 
 @app.route('/docs/api')
 def api_docs():
-    return render_template('api.html')
+    return render_template('docs/api/api.html')
+
+@app.route('/graph')
+def graph():
+    return render_template('templates/graph.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
