df478584675b2a3ecfecd7e6d1989d1cc1468364
marshalkin <artem.marshalkin@gmail.com>
2018-02-08 10:01:10 +0300

    other

--- server/run.py
+++ server/run.py
@@ -7,23 +7,19 @@ app = Eve(settings='settings.py')
 
 loader = jinja2.ChoiceLoader([
     app.jinja_loader,
-    jinja2.FileSystemLoader(os.getcwd()),
+    jinja2.FileSystemLoader(os.getcwd()+'templates'),
 ])
 
 app.jinja_loader = loader
-#app.config['STATIC_FOLDER'] = os.getcwd() + '/static'
+app.config['STATIC_FOLDER'] = os.getcwd() + '/static'
 
 @app.route('/docs/api')
 def api_docs():
-    return render_template('docs/api/api.html')
+    return render_template('api.html')
 
 @app.route('/graph')
 def graph():
-    return render_template('templates/graph.html')
-
-@app.route('/upload')
-def upload():
-    pass
+    return render_template('graph.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
