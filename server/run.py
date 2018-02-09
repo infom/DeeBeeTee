828231c6aadd96694470daef90300588eb77187c
Артем Маршалкин <>
2018-02-10 00:52:20 +0300

    add api docs

--- server/run.py
+++ server/run.py
@@ -3,11 +3,28 @@ import json
 import jinja2
 
 from eve import Eve
+from eve_swagger import swagger, add_documentation
 from flask import render_template, send_from_directory, Response
 
 from util import serializeDatetimeObjJSON, get_file
 
 app = Eve(settings='settings.py')
+app.register_blueprint(swagger)
+
+app.config['SWAGGER_INFO'] = {
+    'title': 'DeeBeeTee API',
+    'version': '1.0',
+    'description': 'This is DeeBeeTee API Specification',
+    'termsOfService': 'http://swagger.io/terms/',
+    'contact': {
+        'email': 'infominfom@gmail.com',
+    },
+    'license': {
+        'name': 'Apache 2.0',
+        'url': 'http://www.apache.org/licenses/LICENSE-2.0.html',
+    },
+    'schemes': ['http', 'https'],
+}
 
 loader = jinja2.ChoiceLoader([
     app.jinja_loader,
