ea4dbd7b13b47b27c7e28b5e39bcc8aeb4e4a674
marshalkin <artem.marshalkin@gmail.com>
2018-02-09 19:39:30 +0300

    other

--- server/run.py
+++ server/run.py
@@ -20,11 +20,12 @@ def getBalance(username):
 
     uid = users.find_one({'username':username}, {'uid': 1, '_id': 0})
 
-    balance = {}
     in_ts = list(transactions.aggregate([{'$group': {'_id': "$to_uid", 'totalAmount': {'$sum': '$amount'}}}]))
     out_ts = list(transactions.aggregate([{'$group': {'_id': "$from_uid", 'totalAmount': {'$sum': '$amount'}}}]))
-    print(in_ts[0]['totalAmount'])
-    return json.dumps(in_ts)
+
+    balance = in_ts[0]['totalAmount'] - out_ts[0]['totalAmount']
+    res = {'balance':balance}
+    return res
 
 @app.route('/docs/api')
 def api_docs():
