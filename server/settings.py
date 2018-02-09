a81cc2400d6a031875dcc77759cbc211f0cd83f2
marshalkin <artem.marshalkin@gmail.com>
2018-02-09 11:06:37 +0300

    create users schema

--- server/settings.py
+++ server/settings.py
@@ -9,78 +9,37 @@ PUBLIC_ITEM_METHODS = ['GET', 'POST']
 
 API_VERSION = 'v1'
 
+DATE_FORMAT = "%m/%d/%y %H:%M:%S"
+
 users = {
-    # Здесь мы описываем модель данных. Для валидации используется модуль Cerberus от автора Eve.
-    # Вы можете ознакомиться с ним в официальной документации модуля http://docs.python-cerberus.org/en/stable/.
-    # Либо прочитать заметки в официальной документации EVE http://python-eve.org/validation.html#validation.
     'schema': {
+        'uid': {
+            'type' : 'integer',
+            'required': True
+        },
         'username': {
             'type': 'string',
-            'minlength': 5,
-            'maxlength': 32,
             'required': True,
             # уникальное поле (индекс не создаётся, просто значение должно быть уникальным)
-            'unique': True,
+            #'unique': True,
         },
-        'firstname': {
+        'name':  {
             'type': 'string',
-            'minlength': 1,
-            'maxlength': 10,
             'required': True,
+            # уникальное поле (индекс не создаётся, просто значение должно быть уникальным)
+            #'unique': True,
         },
-        'lastname': {
-            'type': 'string',
-            'minlength': 1,
-            'maxlength': 15,
-            'required': True,
-        },
-        'role': {
-            'type': 'list', # тип: список
-            'allowed': ["admin", "user"], # разрешаем использовать значения: "author", "contributor"
-        },
-        'location': {
-            'type': 'dict', # тип: словарь
-            # описываем "схему" словаря
-            'schema': {
-                'address': {'type': 'string'},
-                'city': {'type': 'string'}
-            },
-        },
-        'born': {
+        'assing_date': {
             'type': 'datetime',
-        },
-        'active': {
-            'type': 'boolean',
-            'default': True
-        }
-    }
-}
-
-groups = {
-    # Описываем модель данных .
-    'schema': {
-        'title': {
-            'type': 'string',
-            'minlength': 5,
-            'maxlength': 32,
             'required': True,
-            'unique': True
+            # уникальное поле (индекс не создаётся, просто значение должно быть уникальным)
+            #'unique': True,
         },
-        'users': {
-            'type': 'list',  # тип: список
-            'default': [],   # по умолчанию: пустой список
-            # описываем "схему" списка
-            'schema': {
-                'type': 'objectid', # тип данных: objectid
-                # ссылаемся на запись в другой коллекции
-                'data_relation': {
-                    'resource': 'users',  # на ресурс `users` (который мы описали выше)
-                    'field': '_id',  # на поле `_id`
-                    'embeddable': True
-                }
-            }
+        'user_id': {
+            'type' : 'integer',
+            'required': True,
         }
     }
 }
 
-DOMAIN = {'users':users, 'groups':groups}
+DOMAIN = {'users':users}
