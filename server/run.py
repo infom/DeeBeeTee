9a7158bc74656241a55000a95841b71cd7877de1
marshalkin <artem.marshalkin@gmail.com>
2018-02-09 19:12:55 +0300

    other

--- server/run.py
+++ server/run.py
@@ -18,8 +18,8 @@ def getBalance(username):
     transactions = app.data.driver.db['transactions']
 
     uid = users.find_one({'username':username}, {'uid': 1, '_id': 0})
-    #pipe = [{'$group': {'from_uid': uid, 'total': {'$sum': '$amount'}}}]
-    in_ts = transactions.aggregate([{'$group': {'from_uid': uid, 'total': {'$sum': '$amount'}}}])
+    pipe = [{'$group': {'from_uid': uid, 'total': {'$sum': '$amount'}}}]
+    in_ts = transactions.aggregate(pipeline=pipe)
 
     return in_ts
 
