04782c5d3825978b87987c5940af7f2357a799da
Артем Маршалкин <>
2018-02-09 21:25:08 +0300

    other

--- server/run.py
+++ server/run.py
@@ -46,7 +46,8 @@ def getTsByUser(username):
     uid = users.find_one({'username':username}, {'uid': 1, '_id': 0})
 
     fromUidTs = list(transactions.find({'from_uid':uid['uid']}, {'to_uid':1,'date':1, 'description':1, '_id':False}))
-    return Response(json.dumps(fromUidTs), mimetype='application/json')
+    print(fromUidTs)
+    #return Response(json.dumps(fromUidTs), mimetype='application/json')
 
 @app.route('/docs/api')
 def api_docs():
