4180bee51cb777ea7fef92bfa7c0a5c194fd2542
Артем Маршалкин <>
2018-02-09 21:52:05 +0300

    other

--- server/util.py
+++ server/util.py
@@ -1,7 +1,8 @@
+import json
 from datetime import datetime
 
-def serializeDatetimeObjJSON(json):
-    for d in json:
+def serializeDatetimeObjJSON(data):
+    for d in data:
         for key,val in d.items():
             if isinstance(val, datetime):
                 d[key] = '{:%m/%d/%y %H:%M:%S}'.format(val) #you can add different formating here
