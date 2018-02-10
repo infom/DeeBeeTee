ef4bbf8695ede47f562774b8e99357fdaf02e541
Артем Маршалкин <>
2018-02-10 13:17:11 +0300

    add router and change name for main script (dbtApiServer.py)

---
+++ server/dbtApiSerever.py
@@ -0,0 +1,34 @@
+import os
+import json
+import jinja2
+
+from eve import Eve
+from eve_swagger import swagger, add_documentation
+
+app = Eve(settings='settings.py')
+app.register_blueprint(swagger)
+
+app.config['SWAGGER_INFO'] = {
+    'title': 'DeeBeeTee API',
+    'version': '1.0',
+    'description': 'This is DeeBeeTee API Specification',
+    'termsOfService': 'http://swagger.io/terms/',
+    'contact': {
+        'email': 'infominfom@gmail.com',
+    },
+    'license': {
+        'name': 'Apache 2.0',
+        'url': 'http://www.apache.org/licenses/LICENSE-2.0.html',
+    },
+    'schemes': ['http', 'https'],
+}
+
+loader = jinja2.ChoiceLoader([
+    app.jinja_loader,
+    jinja2.FileSystemLoader(os.getcwd()+'/templates'),
+])
+
+app.jinja_loader = loader
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5001, debug=True)
--- server/router.py
+++ server/router.py
@@ -1,37 +1,10 @@
 import os
 import json
-import jinja2
 
-from eve import Eve
-from eve_swagger import swagger, add_documentation
 from flask import render_template, send_from_directory, Response
 
 from util import serializeJSON, get_file
-
-app = Eve(settings='settings.py')
-app.register_blueprint(swagger)
-
-app.config['SWAGGER_INFO'] = {
-    'title': 'DeeBeeTee API',
-    'version': '1.0',
-    'description': 'This is DeeBeeTee API Specification',
-    'termsOfService': 'http://swagger.io/terms/',
-    'contact': {
-        'email': 'infominfom@gmail.com',
-    },
-    'license': {
-        'name': 'Apache 2.0',
-        'url': 'http://www.apache.org/licenses/LICENSE-2.0.html',
-    },
-    'schemes': ['http', 'https'],
-}
-
-loader = jinja2.ChoiceLoader([
-    app.jinja_loader,
-    jinja2.FileSystemLoader(os.getcwd()+'/templates'),
-])
-
-app.jinja_loader = loader
+import dbtApiServer.app as app
 
 def balance(username):
     users = app.data.driver.db['users']
@@ -102,6 +75,3 @@ def graph():
 def serve_static(filename):
     root_dir = os.path.dirname(os.getcwd()+'/server')
     return send_from_directory(os.path.join(root_dir, 'static', 'img'), filename)
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5001, debug=True)
