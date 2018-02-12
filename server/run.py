25f5c29d242fa86ff22b846c3208a1b3eccd40fa
marshalkin <artem.marshalkin@gmail.com>
2018-02-12 10:29:54 +0300

    other

--- server/run.py
+++ server/run.py
@@ -17,6 +17,7 @@ def after_insert_users(items):
     print(items)
 
     for i in items:
+        print(i)
         user = Node('Users', name=i.username)
         graph.create(user)
         print("Create new node "+ i.username)
