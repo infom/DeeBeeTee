efb927024d9d89527184be7a845e954710ec5584
marshalkin <artem.marshalkin@gmail.com>
2018-02-12 14:34:17 +0300

    other

---
+++ server/graphDB.py
@@ -0,0 +1,17 @@
+from neomodel import (config, StructuredNode, StructuredRel, StringProperty, IntegerProperty, DateTimeProperty,
+    UniqueIdProperty, RelationshipTo, RelationshipFrom)
+
+username = os.environ.get('NEO4J_USERNAME')
+password = os.environ.get('NEO4J_PASSWORD')
+
+config.DATABASE_URL = 'bolt://'+username+':'+password+'@194.87.236.140:7687'
+
+class TransactionsRel(StructuredRel):
+    since = DateTimeProperty(default=lambda: datetime.now(pytz.utc))
+    tx = IntegerProperty()
+
+class Person(StructuredNode):
+    uid = UniqueIdProperty()
+    name = StringProperty(unique_index=True)
+    balance = IntegerProperty(index=True, default=0)
+    tx = RelationshipTo('Person', 'TX', model=TransactionsRel)
--- server/run.py
+++ server/run.py
@@ -5,23 +5,7 @@ import jinja2
 import json
 from datetime import datetime, timedelta
 
-from neomodel import (config, StructuredNode, StructuredRel, StringProperty, IntegerProperty, DateTimeProperty,
-    UniqueIdProperty, RelationshipTo, RelationshipFrom)
-
-username = os.environ.get('NEO4J_USERNAME')
-password = os.environ.get('NEO4J_PASSWORD')
-
-config.DATABASE_URL = 'bolt://'+username+':'+password+'@194.87.236.140:7687'
-
-class TransactionsRel(StructuredRel):
-    since = DateTimeProperty(default=lambda: datetime.now(pytz.utc))
-    tx = IntegerProperty()
-
-class Person(StructuredNode):
-    uid = UniqueIdProperty()
-    name = StringProperty(unique_index=True)
-    balance = IntegerProperty(index=True, default=0)
-    tx = RelationshipTo('Person', 'TX', model=TransactionsRel)
+from graphDB import Person
 
 def after_insert_users(items):
 
