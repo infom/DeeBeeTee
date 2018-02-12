fcc1b018e2a1e378c9b03264671613c30b128f8a
marshalkin <artem.marshalkin@gmail.com>
2018-02-12 13:49:00 +0300

    other

--- server/run.py
+++ server/run.py
@@ -12,14 +12,15 @@ password = os.environ.get('NEO4J_PASSWORD')
 
 config.DATABASE_URL = 'bolt://'+username+':'+password+'@194.87.236.140:7687'
 
+class TransactionsRel(StructuredRel):
+    since = DateTimeProperty(default=lambda: datetime.now(pytz.utc))
+    tx = IntegerProperty()
+
 class Person(StructuredNode):
     uid = UniqueIdProperty()
     name = StringProperty(unique_index=True)
     balance = IntegerProperty(index=True, default=0)
-
-class TransactionsRel(StructuredRel):
-    since = DateTimeProperty(default=lambda: datetime.now(pytz.utc))
-    tx = IntegerProperty()
+    tx = RelationshipTo('Person', 'TX', model=TransactionsRel)
 
 def after_insert_users(items):
 
