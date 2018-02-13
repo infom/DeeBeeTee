8b86b93c9c124491c10e7c3606e4ab1a894d4cba
marshalkin <artem.marshalkin@gmail.com>
2018-02-13 15:31:52 +0300

    other

--- server/run.py
+++ server/run.py
@@ -6,7 +6,7 @@ import json
 from datetime import datetime, timedelta
 #from neomodel import OUTGOING, INCOMING
 #from neomodel.match import Traversal
-from graphDB import getBalanceDetails, getUserBalance, createNode
+from graphDB import getBalanceDetails, getUserBalance, createNode, createTransaction
 #from py2neo import Node, NodeSelector, Graph
 #import collections
 
