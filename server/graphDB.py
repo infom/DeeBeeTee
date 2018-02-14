453264a3a6a70642cf27e44e326aa0ad77df645a
marshalkin <artem.marshalkin@gmail.com>
2018-02-14 18:45:03 +0300

    otherother

--- server/graphDB.py
+++ server/graphDB.py
@@ -2,7 +2,9 @@ from pyorient.ogm import Graph, Config
 from pyorient.ogm.property import String, Float, DateTime
 from pyorient.ogm.declarative import declarative_node, declarative_relationship
 # Initialize Graph Database
-graph = Graph(Config.from_url('localhost:2424/DeeBeeTee', 'deebeetee', 'deebeetee'))
+
+graph = Graph(Config.from_url('plocal://localhost:2424//DeeBeeTee', 'deebeetee', 'deebeetee'))
+print(graph)
 
 Node = declarative_node()
 Relationship = declarative_relationship()
