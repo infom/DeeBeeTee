c26037be9784fb8566f00fef29b9a3cec30ee94a
Артем Маршалкин <>
2018-02-14 21:30:08 +0300

    other

--- server/graphClient.py
+++ server/graphClient.py
@@ -39,7 +39,7 @@ def createNewTransaction(data):
     creditb = float(end_node.credit_balance) + tx
     endb = float(end_node.debit_balance) - float(end_node.credit_balance)
 
-    client.command('INSERT INTO Person SET debit_balance='+repr(debitb)+', balance='+repr(startb)+' UPSERT WHERE uid='+repr(from_uid))
+    client.command('INSERT INTO Person SET (debit_balance, balance) VALUE ('+repr(debitb)+','+startb+') UPSERT WHERE uid='+repr(from_uid))
     client.command('INSERT INTO Person SET credit_balance='+repr(creditb)+', balance='+repr(endb)+' UPSERT WHERE uid='+repr(to_uid))
 
     print('Create new transaction')
